7334eef59e0b6d7a270bc6b926c17e2cf407222b
Aggravator <agg@agg.agg>
2016-04-04 16:50:59 +0300

    Models fixed! Migrations executed!

--- algdb/core/models.py
+++ algdb/core/models.py
@@ -11,7 +11,7 @@ class Value(models.Model):
 	double_value=models.FloatField(blank=True,null=True)
 	boolean_value=models.NullBooleanField(blank=True,null=True)
 	string_value=models.CharField(max_length=200,blank=True)
-	date_value=models.DateFiled(blank=True,null=True)
+	date_value=models.DateField(blank=True,null=True)
 	time_value=models.TimeField(blank=True,null=True)
 	datetime_value=models.DateTimeField(blank=True,null=True)
 	class Meta:
@@ -21,47 +21,57 @@ class AlgConstructor(TimeStamp):
 	name=models.CharField(max_length=200)
 	gen_id=models.CharField(max_length=100)
 	data=models.CharField(max_length=3000)
+	desc=models.CharField(max_length=500,blank=True)
 
 class AlgParam(TimeStamp):
 	name=models.CharField(max_length=200)
-	desc=models.CharFiled(max_length=500)
-	gen_id=models.CharField(max_length=100)
+	desc=models.CharField(max_length=500,blank=True)
+	gen_id=models.CharField(max_length=100,blank=True)
 	type=models.PositiveSmallIntegerField()
+
+class Slot(TimeStamp):
+	name=models.CharField(max_length=200)
+	gen_id=models.CharField(max_length=100,blank=True)
+	desc=models.CharField(max_length=500,blank=True)
 
 class Strategy(TimeStamp):
 	name=models.CharField(max_length=200)
-	desc=models.CharField(max_length=500)
-	gen_id=models.CharField(max_length=100)
+	slot=models.ForeignKey(Slot)
+	desc=models.CharField(max_length=500,blank=True)
+	gen_id=models.CharField(max_length=100,blank=True)
 
 class AlgTemplate(TimeStamp):
 	name=models.CharField(max_length=200)
 	strategies=models.ManyToManyField(Strategy)
 	params=models.ManyToManyField(AlgParam)
 	constructor=models.ForeignKey(AlgConstructor,blank=True,null=True)
+	desc=models.CharField(max_length=500,blank=True)
 
 class Algorithm(TimeStamp):
-	template=models.CharField(AlgTemplate)
+	template=models.ForeignKey(AlgTemplate)
 
 class AlgImplementation(TimeStamp):
 	algorithm=models.ForeignKey(Algorithm)
 	author=models.CharField(max_length=200)
 	file=models.CharField(max_length=200)
+	desc=models.CharField(max_length=500,blank=True)
 
 class ParamOfAlg(TimeStamp,Value):
 	algorithm=models.ForeignKey(Algorithm)
-	param=models.ForeignKey(algParam)
+	param=models.ForeignKey(AlgParam)
 
 class TaskParam(TimeStamp):
 	name=models.CharField(max_length=200)
-	desc=models.CharFiled(max_length=500)
+	desc=models.CharField(max_length=500,blank=True)
 	type=models.PositiveSmallIntegerField()
 
 class TaskTemplate(TimeStamp):
 	name=models.CharField(max_length=200)
 	params=models.ManyToManyField(TaskParam)
+	desc=models.CharField(max_length=500,blank=True)
 
 class Task(TimeStamp):
-	template=models.CharField(TaskTemplate)
+	template=models.ForeignKey(TaskTemplate)
 
 class ParamOfTask(TimeStamp,Value):
 	task=models.ForeignKey(Task)
@@ -69,7 +79,7 @@ class ParamOfTask(TimeStamp,Value):
 
 class Computer(TimeStamp):
 	name=models.CharField(max_length=100)
-	desc=models.CharField(max_length=500)
+	desc=models.CharField(max_length=500,blank=True)
 
 class Launch(TimeStamp):
 	task=models.ForeignKey(Task)
